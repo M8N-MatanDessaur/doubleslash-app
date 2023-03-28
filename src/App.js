88bc1075b809095fb491586384f6d609b2696800
Matan Dessaur <matandessaur@m8ndoes.com>
2023-03-28 11:18:22 -0400

    Added Login & Signup forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import MyNotes from './components/Pages/MyNotes';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ButtonDarkLight from './components/UI_Components/ButtonDarkLight';
+import LoginForm from './components/Pages/LoginPage';
+import SignupForm from './components/Pages/SignupPage';
 
 const App =()=> {
   return (
@@ -10,6 +12,8 @@ const App =()=> {
       <ButtonDarkLight/>
       <Routes>
         <Route path='/notes' element={<MyNotes />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/signup' element={<SignupForm />} />
         <Route path='/' element={<Navigate to="/notes"/>} />
       </Routes>
     </Router>
