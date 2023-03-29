7d45a717ee99e269220daa87d8e6031996fb0cb0
Matan Dessaur <matandessaur@m8ndoes.com>
2023-03-29 15:55:54 -0400

    Modified Mutate for more snappy app and added CS language support

--- src/components/Layout_Components/MainLayout.jsx
+++ src/components/Layout_Components/MainLayout.jsx
@@ -16,7 +16,7 @@ export default function MainLayout({openModal, notes, lightMode, stateMode, user
         author: user,
         title: '//',
         body: '',
-        extention: '.txt',
+        extention: 'txt',
         dateCreated: Date.now,
         dateModified: Date.now,
     }
@@ -43,7 +43,7 @@ export default function MainLayout({openModal, notes, lightMode, stateMode, user
         <>
             <TopBar lightMode={lightMode} stateMode={stateMode} profile={profile}/>
             <ContentContainer>
-                {notes.map((note, index) => (note.title.includes(search)||note.title.includes(search.toLowerCase())||note.title.includes(search.toUpperCase())||note.title.includes(search.charAt(0).toUpperCase() + search.slice(1))) ? <NotePreviewCard key={index} cardId={index} openModal={openModal} noteBody={note.body} noteTitle={note.title} noteExtention={note.extention} noteId={note._id}/> : <></>)}
+                {notes.map((note, index) => (note.title.includes(search)||note.title.includes(search.toLowerCase())||note.title.includes(search.toUpperCase())||note.title.includes(search.charAt(0).toUpperCase() + search.slice(1))) ? <NotePreviewCard key={index} cardId={index} openModal={openModal} noteBody={note.body} noteTitle={note.title} noteExtention={note.extention} noteId={note._id} user={user}/> : <></>)}
             </ContentContainer>
             <BottomBar newNote={newNote} searchNote={searchNote}/>
         </>
